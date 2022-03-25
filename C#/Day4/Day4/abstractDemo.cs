422304d55bb237f095a4e89b351f08481c600f3c
Admin <Admin@GURUSHRI>
2022-03-25 19:17:34 +0530

    save

---
+++ C#/Day4/Day4/abstractDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    abstract public class abstractDemo
+    {
+        abstract public void area();
+      
+    }
+
+    public class circle:abstractDemo
+    {
+        public override void area()
+        {
+            Console.WriteLine("InCircle");
+            Console.WriteLine("Circle with radious 20 :"+(3.14*20*20));
+
+        }
+    }
+
+    public class Rectangle:abstractDemo
+    {
+        public override  void area()
+        {
+            Console.WriteLine("InRectangle");
+            Console.WriteLine("Circle with length 20 breadth 10:" + (20 * 10));
+        }
+    }
+    public class Square:abstractDemo
+    {
+        public override void area()
+        {
+            Console.WriteLine("InSquare");
+            Console.WriteLine("Circle with side 20 :" + (20 * 20));
+        }
+    }
+}
---
+++ C#/Day4/customException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    internal class NameException:Exception
+    {
+        public NameException(String m):base(m)
+        {
+            throw new Exception("Invalid Name");
+        }
+
+       public static void mesg(string msg)
+        {
+            throw new Exception("Invalid Name");
+        }
+    }
+}
