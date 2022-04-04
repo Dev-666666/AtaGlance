ff2eb5facb626df286f50afcc9ca27fbea0f4bf7
Devashri_J <101618596+Dev-666666@users.noreply.github.com>
2022-04-05 00:12:49 +0530

    sqlassupd

---
+++ SQL/Assignment2.sql
@@ -0,0 +1,5 @@
+create table customer(cust_id int identity ,cust_name varchar(20),
+country varchar(20),postal_code int,city varchar(20));
+select * from customer;
+create table person(p_id int,p_name varchar(20),birthdate date);
+select * from person;
